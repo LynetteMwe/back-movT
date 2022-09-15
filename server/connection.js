ae3400423beeb9eff934c92745dd882ad7c327f1
Nivlek <kelvinkijanda@gmail.com>
2022-09-15 23:03:15 +0300

    Added parameters to connection using environment vars

--- server/connection.js
+++ server/connection.js
@@ -1,17 +1,11 @@
 const mysql = require("mysql2");
 
-let connection = mysql.createConnection({
-    // port:3306,
-    // host:'localhost',
-    database: "movt",
-    user: "root",
-    password: "root",
-
+const connection = mysql.createConnection({
     port: process.env.DB_PORT,
     host: process.env.DB_HOST,
-    // database: process.env.DB_NAME
-    // user:process.env.DB_USER ,
-    // password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
 });
 
 connection.connect(err => {
