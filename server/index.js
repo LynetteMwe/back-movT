df1b89fc6c18154760b00a569ce9f20d70c4d080
LynetteMwe <lynarichards@gmail.com>
2022-09-18 14:09:09 +0300

    AAdded orders, notifications and trucks routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const connection = require("./connection");
 const driverRoute = require("./routes/drivers");
 const clientRoute = require("./routes/clients");
+const ordersRoute = require('./routes/orders')
+const notificationsRoute = require("./routes/notifications")
+const truckRoute =  require("./routes/trucks")
 
 const app = express();
 
@@ -9,5 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/drivers", driverRoute);
 app.use("/clients", clientRoute);
+app.use("/orders", ordersRoute);
+app.use("/notifications", notificationsRoute)
+app.use("/trucks", truckRoute)
+
 
 module.exports = { app, connection };
