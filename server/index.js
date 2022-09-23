295054ca8698a2797b19b3e3668271f03dcfb897
LynetteMwe <lynarichards@gmail.com>
2022-09-23 17:46:45 +0300

    Passportjs for authentication. Not working as expected.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
-const connection = require("./connection");
+// const connection = require("./connection")(session)
 const driverRoute = require("./routes/drivers");
 const clientRoute = require("./routes/clients");
 const ordersRoute = require('./routes/orders')
 const notificationsRoute = require("./routes/notifications")
 const truckRoute =  require("./routes/trucks")
+
+const authRoute = require('./routes/auth');
+const logger = require('morgan');
+const passport = require('passport');
+const session = require("express-session");
+const connection = require("./connection")
+ 
+// const session = require('express-session');
+// const mysqlStore = require('express-mysql-session')(session);
+// const sessionStore = new mysqlStore(options);
 
 const app = express();
 
@@ -15,6 +25,17 @@ app.use("/clients", clientRoute);
 app.use("/orders", ordersRoute);
 app.use("/notifications", notificationsRoute)
 app.use("/trucks", truckRoute)
+app.use("/auth", authRoute)
+
+app.use(session({
+ secret: 'keyboard cat',
+ resave: false,
+ saveUninitialized: false
+ // store: new connection({ db: 'sessions.db', dir: './var/db' })
+ // store: connection
+ // store: sessionStore
+}));
+app.use(passport.authenticate('session'));
 
 
 module.exports = { app, connection };
