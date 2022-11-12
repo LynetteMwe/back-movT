ead20120bed4ba792f65beb9e6059662d16d2f5e
LynetteMwe <lynarichards@gmail.com>
2022-11-12 14:44:10 +0300

    Modified driver profile route.

--- server/routes/drivers.js
+++ server/routes/drivers.js
@@ -7,49 +7,58 @@ const router = express.Router();
 
 // Get single user by id
 router.get("/:id(\\d+)", (req, res, next) => {
-    Driver.findByPk(req.params.id)
-        .then(user => {
-            if (!user)
-                return res.status(404).json({
-                    status: res.statusCode, // Not Found
-                    error: "User not found!",
-                });
-            res.json(getUser(user));
-        })
-        .catch(error => serverError(res, error));
+	Driver.findByPk(req.params.id)
+		.then((user) => {
+			if (!user)
+				return res.status(404).json({
+					status: res.statusCode, // Not Found
+					error: "User not found!",
+				});
+			res.json(getUser(user));
+		})
+		.catch((error) => serverError(res, error));
 });
 
 // Get single user by id
 router.get("/profile", (req, res, next) => {
-    return res.status(200).json(getUser(req.user));
+	const driver = {
+		id: req.user.id,
+		username: req.user.username,
+		email: req.user.email,
+		contact: req.user.contact,
+		carType: req.user.carType,
+		vehicle_plate_no: req.user.vehicle_plate_no,
+		type: req.user.type,
+	};
+	return res.status(200).json(driver);
 });
 
 // Get all users
 router.get("/", async (req, res) => {
-    const _ = await Driver.findAll();
-    users = _.map(user => getUser(user));
-    res.status(200).json(users);
+	const _ = await Driver.findAll();
+	users = _.map((user) => getUser(user));
+	res.status(200).json(users);
 });
 
 // Create a new user
 router.post("/", (req, res, next) => {
-    const { username, contact, email, password } = req.body;
-    Driver.create({
-        username,
-        contact,
-        email,
-        password,
-        token: generateToken(email),
-    })
-        .then(user => {
-            if (!user)
-                return res.status(400).json({
-                    status: res.statusCode, // Bad Request
-                    error: "Provide username, contact, email, password",
-                });
-            res.json(getUser(user, true));
-        })
-        .catch(error => serverError(res, error));
+	const { username, contact, email, password } = req.body;
+	Driver.create({
+		username,
+		contact,
+		email,
+		password,
+		token: generateToken(email),
+	})
+		.then((user) => {
+			if (!user)
+				return res.status(400).json({
+					status: res.statusCode, // Bad Request
+					error: "Provide username, contact, email, password",
+				});
+			res.json(getUser(user, true));
+		})
+		.catch((error) => serverError(res, error));
 });
 
 // Update a user
